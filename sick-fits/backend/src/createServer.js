abe2b4d903ec6ee27a1dc1528e443e268efdd9a1
elliott gonzales <egonzales@elgonzales.local>
2019-08-16 14:40:00 -0600

    Added the file schema.graphql

---
+++ sick-fits/backend/src/createServer.js
@@ -0,0 +1,15 @@
+const { GraphQLServer } = require('graphql-yoga');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const db = require('./db');
+
+// Create the GraphQL Yoga Server
+
+function createServer() {
+  return new GraphQLServer() {
+    typeDefs: 
+
+  });
+}
+
+module.exports = createServer;
